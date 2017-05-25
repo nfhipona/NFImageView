dcb829b441196416a07ac58bee2b57a1372386bc
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2017-05-25 16:34:27 +0800

    Update pods

--- Example/NFImageView/ViewController.swift
+++ Example/NFImageView/ViewController.swift
@@ -34,14 +34,14 @@ class ViewController: UIViewController {
         // NOTE: Test with network conditioner to fully see the effect.
         
         // used for image view with blur effect
-        let thumbnail = "https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-9/13529069_10202382982213334_6754953260473113193_n.jpg?oh=28c0f3e751a9177e5ca0afaf23be919e&oe=57F9EEF9"
-        let largeImage = "https://scontent.fmnl4-4.fna.fbcdn.net/t31.0-8/13584845_10202382982333337_2990050100601729771_o.jpg"
+        /*let thumbnail = "https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-9/13529069_10202382982213334_6754953260473113193_n.jpg?oh=28c0f3e751a9177e5ca0afaf23be919e&oe=57F9EEF9"
+        let largeImage = "https://scontent.fmnl4-4.fna.fbcdn.net/t31.0-8/13584845_10202382982333337_2990050100601729771_o.jpg"*/
         
         let thumbnail2 = "https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-9/13599954_10202383100616294_8094668176870097608_n.jpg?oh=a97f20061fdd8f90c19fe027119f399e&oe=57F2623E"
         let largeImage2 = "https://scontent.fmnl4-4.fna.fbcdn.net/t31.0-8/13559093_10202383101056305_5896200803868079783_o.jpg"
         
-        let largeImageSpinner = "https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-0/p206x206/13438921_10202383108576493_2344489318373614654_n.jpg?oh=f2dd51ffa63c2d1f1a91de2b231ebd46&oe=57E9AA07"
-        let largeImageProgress = "https://scontent.fmnl4-4.fna.fbcdn.net/t31.0-8/13613290_10202383101656320_342503551114701717_o.jpg"
+        /*let largeImageSpinner = "https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.0-0/p206x206/13438921_10202383108576493_2344489318373614654_n.jpg?oh=f2dd51ffa63c2d1f1a91de2b231ebd46&oe=57E9AA07"
+        let largeImageProgress = "https://scontent.fmnl4-4.fna.fbcdn.net/t31.0-8/13613290_10202383101656320_342503551114701717_o.jpg"*/
         
 //        imageView1.loadingEnabled = false
         imageView1.loadingType = .spinner
