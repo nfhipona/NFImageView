eee97e662805cab7a02c767c6e15b4d28bc8a764
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-02-24 16:12:21 +0800

    Remove subclassable type in Image Cache API

--- NFImageView/Classes/NFImageCacheAPI.swift
+++ NFImageView/Classes/NFImageCacheAPI.swift
@@ -10,18 +10,18 @@ import Foundation
 import AlamofireImage
 import Alamofire
 
-open class NFImageCacheAPI: NSObject {
+public class NFImageCacheAPI: NSObject {
     
     public static let shared = NFImageCacheAPI()
     
-    open lazy var imageDownloadQueue: DispatchQueue = {
+    public lazy var imageDownloadQueue: DispatchQueue = {
         DispatchQueue(label: "com.NFImageDownloadQueue.concurrent", attributes: DispatchQueue.Attributes.concurrent)
     }()
     
     fileprivate let imageRequestCache: AutoPurgingImageCache = {
         AutoPurgingImageCache(memoryCapacity: 150 * 1024 * 1024, preferredMemoryUsageAfterPurge: 60 * 1024 * 1024)
     }()
-    
+
     fileprivate lazy var imageDownloader: ImageDownloader = {
         ImageDownloader(configuration: ImageDownloader.defaultURLSessionConfiguration(), downloadPrioritization: .lifo, maximumActiveDownloads: 4, imageCache: self.imageRequestCache)
     }()
@@ -31,7 +31,7 @@ open class NFImageCacheAPI: NSObject {
     /**
      * Asynchronously download and cache image from a requested URL using AlamofireImage configuration
      */
-    open func download(imageURL requestURL: URL, completion: ImageDownloader.CompletionHandler? = nil) -> RequestReceipt? {
+    public func download(imageURL requestURL: URL, completion: ImageDownloader.CompletionHandler? = nil) -> RequestReceipt? {
         let request = URLRequest(url: requestURL)
         
         return imageDownloader.download(request, completion: completion)
@@ -40,7 +40,7 @@ open class NFImageCacheAPI: NSObject {
     /**
      * Asynchronously download and cache image from a requested URL using AlamofireImage configuration with progress handler
      */
-    open func downloadWithProgress(imageURL requestURL: URL, progress: ImageDownloader.ProgressHandler?, completion: ImageDownloader.CompletionHandler?) -> RequestReceipt? {
+    public func downloadWithProgress(imageURL requestURL: URL, progress: ImageDownloader.ProgressHandler?, completion: ImageDownloader.CompletionHandler?) -> RequestReceipt? {
         let request = URLRequest(url: requestURL)
         
         return imageDownloader.download(request, progress: progress, completion: completion)
@@ -49,7 +49,7 @@ open class NFImageCacheAPI: NSObject {
     /**
      * Check image cache for url request
      */
-    open func imageContentInCacheStorage(forURL requestURL: URL, identifier: String? = nil) -> UIImage? {
+    public func imageContentInCacheStorage(forURL requestURL: URL, identifier: String? = nil) -> UIImage? {
         
         let request = URLRequest(url: requestURL)
         return imageRequestCache.image(for: request, withIdentifier: identifier)
@@ -58,7 +58,7 @@ open class NFImageCacheAPI: NSObject {
     /**
      * Cache image for url request
      */
-    open func cacheImage(_ image: UIImage, forURL requestURL: URL, identifier: String? = nil) {
+    public func cacheImage(_ image: UIImage, forURL requestURL: URL, identifier: String? = nil) {
         
         let request = URLRequest(url: requestURL)
         imageRequestCache.add(image, for: request, withIdentifier: identifier)
@@ -67,7 +67,7 @@ open class NFImageCacheAPI: NSObject {
     /**
      * Remove image from cache for url request
      */
-    open func removeCachedImage(forURL requestURL: URL, identifier: String? = nil) {
+    public func removeCachedImage(forURL requestURL: URL, identifier: String? = nil) {
         
         let request = URLRequest(url: requestURL)
         imageRequestCache.removeImage(for: request, withIdentifier: identifier)
